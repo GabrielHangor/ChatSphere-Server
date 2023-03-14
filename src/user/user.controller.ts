e84477ff7ec8ff4105103c0b13eb47c309c194f2
Gabriel <gaba93@yandex.ru>
2023-03-14 13:41:00 +0300

    user module - login method (compare hashes)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './model/dto/create-user.dto';
+import { LoginUserDto } from './model/dto/login-user.dto';
 
 @Controller('users')
 export class UserController {
@@ -14,5 +15,10 @@ export class UserController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.userService.login(loginUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateUserDto } from './model/dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { IPaginationOptions, paginate } from 'nestjs-typeorm-paginate';
+import { LoginUserDto } from './model/dto/login-user.dto';
 
 @Injectable()
 export class UserService {
@@ -16,9 +17,9 @@ export class UserService {
   }
 
   async create(createUserDto: CreateUserDto) {
-    const doesEmailExist = await this.checkIfEmailExists(createUserDto.email);
+    const foundUser = await this.findByEmail(createUserDto.email);
 
-    if (doesEmailExist) {
+    if (foundUser) {
       throw new HttpException(`Email ${createUserDto.email} already exists`, HttpStatus.CONFLICT);
     }
 
@@ -34,11 +35,34 @@ export class UserService {
     return createdUserWithoutPassword;
   }
 
-  private async checkIfEmailExists(email: string) {
-    return this.userRepository.findOne({ where: { email } });
+  async login(loginUserDto: LoginUserDto) {
+    const foundUser = await this.findByEmail(loginUserDto.email);
+
+    if (!foundUser) {
+      throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
+    }
+
+    const isPasswordValid = await this.validatePassword(loginUserDto.password, foundUser.password);
+
+    if (!isPasswordValid) {
+      throw new HttpException('Invalid password', HttpStatus.UNAUTHORIZED);
+    }
+
+    return true;
+  }
+
+  private findByEmail(email: string) {
+    return this.userRepository.findOne({
+      where: { email },
+      select: ['id', 'email', 'username', 'password'],
+    });
   }
 
   private hashPassword(password: string) {
     return bcrypt.hash(password, 12);
+  }
+
+  private validatePassword(password: string, storedPassword: string) {
+    return bcrypt.compare(password, storedPassword);
   }
 }

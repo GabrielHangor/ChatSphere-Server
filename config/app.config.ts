9dfe07bfd92cc29970bf048704d77f762691303c
Gabriel <gaba93@yandex.ru>
2023-04-04 15:23:10 +0300

    fix: db port default value removed

--- config/app.config.ts
+++ config/app.config.ts
@@ -3,7 +3,7 @@ export default () => ({
   jwtSecret: process.env.JWT_SECRET,
   database: {
     host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    port: parseInt(process.env.DATABASE_PORT, 10),
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,

f4cee14c448af126ff87e8d761fb2eb392bc0e34
Gabriel <gaba93@yandex.ru>
2023-04-04 17:54:02 +0300

    fix: added cors support for domain

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
-
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: 'https://gabrielhangor.github.io/ChatSphere-Client', methods: '*' });
 
   app.useGlobalPipes(
     new ValidationPipe({

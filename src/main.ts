b4ede7eb3e2e49e7b3d90b04175334533acbde33
Gabriel <gaba93@yandex.ru>
2023-04-04 10:33:18 +0300

    feat: added support for custom process.env PORT for render hosting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
@@ -14,6 +15,6 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
